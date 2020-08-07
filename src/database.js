42b635e62dcc2e5ab0a77a449c9ea313b11af020
hadiazb <hadiazb@unal.edu.co>
2020-08-07 18:40:21 -0500

    Configuración de la base de datos

--- src/database.js
+++ src/database.js
@@ -1,11 +1,20 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'hdrFKDSDNr',
-  database: 'company',
+	host: 'us-cdbr-east-02.cleardb.com' || 'localhost',
+	user: 'b7fc3bad1faa19' || 'root',
+	password: 'a5a38c27' || 'hdrFKDSDNr',
+	database: 'heroku_d28241b7e155f29' || 'company',
 });
+
+// CLEARDB_DATABASE_URL: mysql://b7fc3bad1faa19:a5a38c27@us-cdbr-east-02.cleardb.com/heroku_d28241b7e155f29?reconnect=true
+
+// nombre de usuario: b7fc3bad1faa19
+// contraseña: a5a38c27
+// host: us-cdbr-east-02.cleardb.com
+// nombre_db: heroku_d28241b7e155f29
+
+// DATABASE_URL: mysql://b7fc3bad1faa19:a5a38c27@us-cdbr-east-02.cleardb.com/heroku_d28241b7e155f29?reconnect=true
 
 mysqlConnection.connect(function (err) {
 	if (err) {
