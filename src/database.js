fea31bcbcb2a92682b70ea64695198121c917f32
hadiazb <hadiazb@unal.edu.co>
2020-08-07 21:40:58 -0500

    Cambio

--- src/database.js
+++ src/database.js
@@ -12,13 +12,6 @@ const mysqlConnection = mysql.createConnection({
 	// database: 'heroku_d28241b7e155f29',
 });
 
-// CLEARDB_DATABASE_URL: mysql://b7fc3bad1faa19:a5a38c27@us-cdbr-east-02.cleardb.com/heroku_d28241b7e155f29?reconnect=true
-
-// nombre de usuario: b7fc3bad1faa19
-// contraseña: a5a38c27
-// host: us-cdbr-east-02.cleardb.com
-// nombre_db: heroku_d28241b7e155f29
-
 // DATABASE_URL: mysql://b7fc3bad1faa19:a5a38c27@us-cdbr-east-02.cleardb.com/heroku_d28241b7e155f29?reconnect=true
 
 mysqlConnection.connect(function (err) {
