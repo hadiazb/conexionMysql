338e8c0b5e6829572ee9b0d354baa32601928a22
hadiazb <hadiazb@unal.edu.co>
2020-08-07 21:28:20 -0500

    Cambio

--- src/database.js
+++ src/database.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-	// host: 'localhost',
-	// user: 'root',
-	// password: 'hdrFKDSDNr',
-	// database: 'company',
+	host: 'us-cdbr-east-02.cleardb.com' || 'localhost',
+	user: 'b7fc3bad1faa19' || 'root',
+	password: 'a5a38c27' || 'hdrFKDSDNr',
+	database: 'heroku_d28241b7e155f29' || 'company',
 
-	host: 'us-cdbr-east-02.cleardb.com' ,
-	user: 'b7fc3bad1faa19' ,
-	password: 'a5a38c27' ,
-	database: 'heroku_d28241b7e155f29' ,
+	// host: 'us-cdbr-east-02.cleardb.com',
+	// user: 'b7fc3bad1faa19',
+	// password: 'a5a38c27',
+	// database: 'heroku_d28241b7e155f29',
 });
 
 // CLEARDB_DATABASE_URL: mysql://b7fc3bad1faa19:a5a38c27@us-cdbr-east-02.cleardb.com/heroku_d28241b7e155f29?reconnect=true
